527e14ed457cb57bacbb46ca249778c6960210e3
Grégori MIGNEROT <gmnrt@laposte.net>
2024-01-24 19:46:43 +0100

    Database config with environment variables

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'plsres';
-$username = 'root';
-$password = '';
+$host     = empty(getenv("MYSQL_HOST"))    ? "localhost" : getenv("MYSQL_HOST");
+$dbname   = empty(getenv("MYSQL_DBNAME"))  ? "plsres"    : getenv("MYSQL_DBNAME");  
+$username = empty(getenv("MYSQL_USERNAME"))? "root"      : getenv("MYSQL_USERNAME");
+$password = empty(getenv("MYSQL_PASSWORD"))? ""          : getenv("MYSQL_PASSWORD");
 
 try {
     $conn = new mysqli($host, $username, $password, $dbname);
